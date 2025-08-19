4ce783471868337a9c0864bb064e692af108b961
Aseem7905H <144022226+Aseem7905H@users.noreply.github.com>
2025-08-19 22:06:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// DATABASE_URL ka format hota hai: mysql://user:password@host:port/database
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool.promise();
